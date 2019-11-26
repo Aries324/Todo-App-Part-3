0d8b09cbc135c32527c30df347fb6784b02b14c3
Tiffany McLean <tamclean324@gmail.com>
2019-11-25 21:36:47 -0500

    creates toggle event handler for checkmarks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,59 @@ import todosList from "./todos.json";
 class App extends Component {
   state = {
     todos: todosList
+
   };
+
+  handleToggleTodo = (event, todoIdToToggle) => {
+    console.log ('checkbox was clicked')
+
+      //creates a copy and modify the copy with map
+      const newTodoList = this.state.todos.map(todo => {
+        if( todo.id === todoIdToToggle) {
+
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+
+      //overwrite the original with the modified copy
+
+      this.setState({ todos: newTodoList });
+
+
+  };
+
+
+  handleCreateTodo = event => {
+    //use public class syntax to permanently bind this to App
+
+    if(event.key === 'Enter') {
+      const newTodo = {
+        "userId": 1,
+        "id": Math.floor(Math.random() * 1000000000),
+        "title": event.target.value,
+        "completed": false
+      };
+      //immutability pattern--don't modify an existing version of the data
+      //create a copy
+
+      //you can use spread syntax instead of slice
+      //const newTodoList = [...this.state.todos]
+      const newTodoList = this.state.todos.slice()
+      //modify the copy
+      newTodoList.push(newTodo);
+
+      //overwrite the original with the modified copy
+
+      this.setState({ todos: newTodoList });
+
+      event.target.value = "";
+    }
+
+  };
+
+ 
+
   render() {
     return (
       <section className="todoapp">
@@ -14,10 +66,11 @@ class App extends Component {
           <input
             className="new-todo"
             placeholder="What needs to be done?"
-            autofocus
+            autoFocus
+            onKeyDown = {this.handleCreateTodo}
           />
         </header>
-        <TodoList todos={this.state.todos} />
+        <TodoList handleToggleTodo = {this.handleToggleTodo} todos={this.state.todos} />
         <footer className="footer">
           <span className="todo-count">
             <strong>0</strong> item(s) left
@@ -30,6 +83,7 @@ class App extends Component {
 }
 
 class TodoItem extends Component {
+  
   render() {
     return (
       <li className={this.props.completed ? "completed" : ""}>
@@ -38,6 +92,8 @@ class TodoItem extends Component {
             className="toggle"
             type="checkbox"
             checked={this.props.completed}
+            onChange = {event => this.props.handleToggleTodo(event, this.props.id)
+            }
           />
           <label>{this.props.title}</label>
           <button className="destroy" />
@@ -53,7 +109,8 @@ class TodoList extends Component {
       <section className="main">
         <ul className="todo-list">
           {this.props.todos.map(todo => (
-            <TodoItem title={todo.title} completed={todo.completed} />
+            <TodoItem handleToggleTodo  = {this.props.handleToggleTodo} title={todo.title} completed={todo.completed}
+            id = {todo.id} />
           ))}
         </ul>
       </section>
