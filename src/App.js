75b602a86c3e271ce3e36d800b41095397d81313
Tiffany McLean <tamclean324@gmail.com>
2019-12-02 23:33:06 -0500

    adds navigation between tabs of'all, active, and completed

--- src/App.js
+++ src/App.js
@@ -1,88 +1,73 @@
 import React, { Component } from "react";
-import TodoList from './TodoList'
+import TodoList from "./TodoList";
 import "./index.css";
 import todosList from "./todos.json";
-
+import { Route, Link} from 'react-router-dom'
 class App extends Component {
   state = {
     todos: todosList
-
   };
-
- 
 
   handleClearCompletedTodos = event => {
- 
     //copy the state to be modified
 
-    const newTodoList = this.state.todos.filter( todo => {
+    const newTodoList = this.state.todos.filter(todo => {
       //expecting you to return either true or false
-      if(todo.completed === true) {
+      if (todo.completed === true) {
         return false;
       }
       return true;
-    })
+    });
 
     //overwrite the original with copy
-    this.setState({todos: newTodoList});
-   
-
-};
-
-  handleDeleteTodo = (event, todoIdToDelete) => {
- 
-    //copy the state to be modified
-
-    const newTodoList = this.state.todos.filter( todo => {
-      //expecting you to return either true or false
-      if(todo.id === todoIdToDelete) {
-        return false;
-      }
-      return true;
-    })
-
-    //overwrite the original with copy
-    this.setState({todos: newTodoList});
-   
-
-};
-
-
-  handleToggleTodo = (event, todoIdToToggle) => {
-  
-      //creates a copy and modify the copy with map
-      const newTodoList = this.state.todos.map(todo => {
-        if( todo.id === todoIdToToggle) {
-
-          return { ...todo, completed: !todo.completed };
-        }
-        return todo;
-      });
-
-      //overwrite the original with the modified copy
-
-      this.setState({ todos: newTodoList });
-
-
+    this.setState({ todos: newTodoList });
   };
 
+  handleDeleteTodo = (event, todoIdToDelete) => {
+    //copy the state to be modified
+
+    const newTodoList = this.state.todos.filter(todo => {
+      //expecting you to return either true or false
+      if (todo.id === todoIdToDelete) {
+        return false;
+      }
+      return true;
+    });
+
+    //overwrite the original with copy
+    this.setState({ todos: newTodoList });
+  };
+
+  handleToggleTodo = (event, todoIdToToggle) => {
+    //creates a copy and modify the copy with map
+    const newTodoList = this.state.todos.map(todo => {
+      if (todo.id === todoIdToToggle) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+
+    //overwrite the original with the modified copy
+
+    this.setState({ todos: newTodoList });
+  };
 
   handleCreateTodo = event => {
     //use public class syntax to permanently bind this to App
 
-    if(event.key === 'Enter') {
+    if (event.key === "Enter") {
       const newTodo = {
-        "userId": 1,
-        "id": Math.floor(Math.random() * 1000000000),
-        "title": event.target.value,
-        "completed": false
+        userId: 1,
+        id: Math.floor(Math.random() * 1000000000),
+        title: event.target.value,
+        completed: false
       };
       //immutability pattern--don't modify an existing version of the data
       //create a copy
 
       //you can use spread syntax instead of slice
       //const newTodoList = [...this.state.todos]
-      const newTodoList = this.state.todos.slice()
+      const newTodoList = this.state.todos.slice();
       //modify the copy
       newTodoList.push(newTodo);
 
@@ -92,10 +77,7 @@ class App extends Component {
 
       event.target.value = "";
     }
-
   };
-
- 
 
   render() {
     return (
@@ -106,34 +88,65 @@ class App extends Component {
             className="new-todo"
             placeholder="What needs to be done?"
             autoFocus
-            onKeyDown = {this.handleCreateTodo}
+            onKeyDown={this.handleCreateTodo}
           />
         </header>
-        <TodoList 
-        handleToggleTodo = {this.handleToggleTodo} 
-        handleDeleteTodo = {this.handleDeleteTodo}
-        todos={this.state.todos} />
+        <Route exact path = "/">
+        <TodoList
+          handleToggleTodo={this.handleToggleTodo}
+          handleDeleteTodo={this.handleDeleteTodo}
+          todos={this.state.todos}
+        />
+        </Route>
+        <Route exact path = "/active">
+        <TodoList
+          handleToggleTodo={this.handleToggleTodo}
+          handleDeleteTodo={this.handleDeleteTodo}
+          todos={this.state.todos.filter(todo => todo.completed === false)}
+        />
+        </Route>
+        <Route exact path = "/completed">
+        <TodoList
+          handleToggleTodo={this.handleToggleTodo}
+          handleDeleteTodo={this.handleDeleteTodo}
+          todos={this.state.todos.filter(todo =>todo.completed ===true)}
+        />
+        </Route>
         <footer className="footer">
           <span className="todo-count">
-            <strong>{this.state.todos.filter(todo => {
-              if(todo.completed === false) {
-        return true;
-      }
-      return false;
-            }).length
-            }
-            </strong> 
-            item(s) left
+            <strong>
+              {
+                this.state.todos.filter(todo => {
+                  if (todo.completed === false) {
+                    return true;
+                  }
+                  return false;
+                }).length
+              }
+            </strong>
+             item(s) left
           </span>
-          <button onClick = {this.handleClearCompletedTodos} className="clear-completed">Clear completed</button>
+          <ul className="filters">
+            <li>
+              <Link to="/">All</Link>
+            </li>
+            <li>
+              <Link to="/active">Active</Link>
+            </li>
+            <li>
+              <Link to="/completed">Completed</Link>
+            </li>
+          </ul>
+          <button
+            onClick={this.handleClearCompletedTodos}
+            className="clear-completed"
+          >
+            Clear completed
+          </button>
         </footer>
       </section>
     );
   }
 }
-
-
-
-
 
 export default App;
